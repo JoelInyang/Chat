2dd593ac624c6f4f0ce7611628b7e72a553ccd3d
Joel Inyang <joelinyang2003@gmail.com>
2024-05-02 07:40:12 +0100

    chat

--- Chat/settings.py
+++ Chat/settings.py
@@ -1,9 +1,13 @@
 
 
 from pathlib import Path
+
 import environ
 env = environ.Env()
 environ.Env.read_env()
+
+import env
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
