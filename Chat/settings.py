42dd24d49f37da8db9ccc85e74df998d4d47dc1a
Joel Inyang <joelinyang2003@gmail.com>
2024-05-02 11:08:33 +0100

    chat

--- Chat/settings.py
+++ Chat/settings.py
@@ -129,5 +129,5 @@ import firebase_admin
 from firebase_admin import credentials
 
 # Initialize Firebase Admin SDK
-cred = credentials.Certificate(".\chat-3bdc8-firebase-adminsdk-pv61d-b4abcf2c71.json")
+cred = credentials.Certificate("C:\Users\JOEL\Desktop\Chat\Chat\chat-3bdc8-firebase-adminsdk-pv61d-b4abcf2c71.json")
 firebase_admin.initialize_app(cred)
