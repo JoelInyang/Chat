1459b2e9d1ba8557f207cb189ce1f08efde30ee9
Joel Inyang <joelinyang2003@gmail.com>
2024-05-02 12:43:49 +0100

    chat

--- Chat/settings.py
+++ Chat/settings.py
@@ -36,11 +36,13 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     
     'channels',
+    'corsheaders',
     
     'users',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -122,6 +124,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 # settings.py
