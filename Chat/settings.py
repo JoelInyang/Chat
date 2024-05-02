34f2f57dc8ac2750d0e5477714624f9c77ba5735
Joel Inyang <joelinyang2003@gmail.com>
2024-05-02 05:46:28 +0100

    websocket

--- Chat/settings.py
+++ Chat/settings.py
@@ -1,7 +1,7 @@
 
 
 from pathlib import Path
-
+import env
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lyeqy@wr^1s^t%)49k7+eq-@!(7$zi0*%)4&@c@-9a3fwi@i2t'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
